28ef4800d91db4d6640e0d442b9d64bd82ef5fdf
ralf979 <ralf979@gmail.com>
2023-05-02 12:07:51 +0000

    BlackIron: Rebrand to Black Iron Settings
    Special Thanks to Nitrojen Project and his lead @xyyx

---
+++ src/com/blackiron/settings/fragments/NotificationSettings.java
@@ -0,0 +1,23 @@
+package com.blackiron.settings.fragments;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import android.os.Bundle;
+import com.android.settings.R;
+
+import com.android.settings.SettingsPreferenceFragment;
+
+public class NotificationSettings extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        addPreferencesFromResource(R.xml.blackiron_settings_notifications);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.CUSTOM_SETTINGS;
+    }
+}
